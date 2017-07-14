a35849806163085c968147506bf49a5a0acca068
Avinash Katore <avinashkatore89@gmail.com>
2017-07-14 16:24:04 +0530

    script changes

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -2,7 +2,7 @@
 
 setCrons(){
     crontmp=/tmp/cron.skybot
-    echo "@reboot /opt/bin/init" >> $crontmp 2>&1
+    echo "@reboot sudo /opt/bin/init" >> $crontmp 2>&1
     echo "@reboot sudo python /opt/skybot/scripts/remove_older_logs.py" >> $crontmp 2>&1
     echo "0 0 1 * * ? sudo python /opt/skybot/scripts/remove_older_logs.py" >> $crontmp 2>&1
     echo "*/1 * * * * /opt/bin/scheduler" >> $crontmp
