8cce106e348bfa673fa78a096f428fcaccd5a19d
Avinash Katore <avinashkatore89@gmail.com>
2019-02-26 17:31:40 +0530

    Reboot script added.

--- scripts/crontab.sh
+++ scripts/crontab.sh
@@ -5,6 +5,7 @@ setCrons(){
     echo "@reboot sudo /opt/bin/init" >> $crontmp 2>&1
     echo "@reboot sudo python /opt/skybot/scripts/remove_older_logs.py" >> $crontmp 2>&1
     echo "0 0 1 * * ? sudo python /opt/skybot/scripts/remove_older_logs.py" >> $crontmp 2>&1
+    echo "5 * * * * ? sudo python /opt/skybot/scripts/reboot.py" >> $crontmp 2>&1
     echo "*/1 * * * * /opt/bin/scheduler" >> $crontmp
     if [ -f "/usr/bin/weavedstart.sh" ]; then
         echo "@reboot /usr/bin/weavedstart.sh" >> $crontmp
